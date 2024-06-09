c9074544d4d675701b03e313e4ac3f25ebec0c17
hdatdoo <datdh@omegatheme.com>
2024-06-09 18:18:54 +0700

    init

--- starter-blog/src/App.tsx
+++ starter-blog/src/App.tsx
@@ -1,17 +1,7 @@
-import Header from 'components/Header'
 import Blog from 'pages/blog'
-import { Fragment } from 'react'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  return (
-    <Fragment>
-      <ToastContainer />
-      <Header />
-      <Blog />
-    </Fragment>
-  )
+  return <Blog />
 }
 
 export default App
--- starter-blog/src/pages/blog/components/PostList/PostList.tsx
+++ starter-blog/src/pages/blog/components/PostList/PostList.tsx
@@ -1,26 +1,6 @@
-import { useDeletePostMutation, useGetPostsQuery } from 'pages/blog/blog.service'
-import { startEditPost } from 'pages/blog/blog.slice'
-import { useAppDispatch } from 'store'
-import { Post } from 'types/blog.type'
 import PostItem from '../PostItem'
-import SkeletonPost from '../SkeletonPost'
 
 export default function PostList() {
-  const dispatch = useAppDispatch()
-  // isLoading: for first load
-  // isFetching: for pagination
-  const { data, isFetching } = useGetPostsQuery()
-  // delete post
-  const [deletePost, deletePostResult] = useDeletePostMutation()
-
-  const startEdit = (post: Post) => {
-    dispatch(startEditPost(post.id))
-  }
-
-  const handleDeletePost = async (post: Post) => {
-    await deletePost(post.id).unwrap()
-  }
-
   return (
     <div className='bg-white py-6 sm:py-8 lg:py-12'>
       <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
@@ -31,17 +11,10 @@ export default function PostList() {
           </p>
         </div>
         <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
-          {isFetching ? (
-            <>
-              <SkeletonPost />
-              <SkeletonPost />
-            </>
-          ) : (
-            data?.length &&
-            data.map((post: Post) => (
-              <PostItem key={post.id} post={post} startEdit={startEdit} onDelete={handleDeletePost} />
-            ))
-          )}
+          <PostItem />
+          <PostItem />
+          <PostItem />
+          <PostItem />
         </div>
       </div>
     </div>
